c7d80ef2f0b29106f5c3183075ff896ea0917a6b
KikeCamarenaLS <49732143+KikeCamarenaLS@users.noreply.github.com>
2022-07-18 15:06:32 -0500

    cambios

--- app/Http/Controllers/VentasController.php
+++ app/Http/Controllers/VentasController.php
@@ -488,6 +488,22 @@ $bitacora=DB::select('insert into tb_bitacora (ID_Bitacora,ID_EMPLEADO,nomemplea
   return view('Terrenos.Ventas.SanPabloVIII',compact('proyectos','lotes','id_proy'));
 }
 
+public function ventalotesView38(){
+  $proyectos=DB::select('SELECT * FROM cat_proyectos ORDER BY PROYECTO ASC');
+  $lotes=DB::select('SELECT * FROM proyectoLote where proyecto=24');
+  $id_proy=24;
+
+  date_default_timezone_set("America/Mexico_City");
+$fechaPHP=date('Y-m-d H:i:s');
+$idUsuarioSistema = Auth::user()->id;
+$nombreUsuarioSistema=DB::select('select CONCAT(Nombre," ",Apellido_Paterno," ",Apellido_Materno)as nombre from users where id='.$idUsuarioSistema);
+$bitacora=DB::select('insert into tb_bitacora (ID_Bitacora,ID_EMPLEADO,nomempleado,created_at, CVE_MOVIMIENTO, MOVIMIENTO) values (null,"'.$idUsuarioSistema.'","'.$nombreUsuarioSistema[0]->nombre.'","'.$fechaPHP.'",6," Ingreso a San Pablo VIII " )');
+
+  return view('Terrenos.Ventas.SanPabloVIII',compact('proyectos','lotes','id_proy'));
+}
+
+
+
 
 
 
